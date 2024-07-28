4edcd38cc5ae7e7a776c65af3b91344d8d14ff1a
JolyMolyf <vs2001dor@gmail.com>
2024-07-28 19:28:43 +0200

    added correct server url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://szkolenia.ricg.eu',
+  url: 'https://szkolenia.ricg.eu',
   app: {
     keys: env.array('APP_KEYS'),
   },
